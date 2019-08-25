5e0501c50414c244b6f1c50814ce0da2dc1c2779
Robert Molina <rmolinamir@gmail.com>
2019-08-25 01:27:27 -0400

    removed useIsReady

--- pages/about-us.js
+++ pages/about-us.js
@@ -1,18 +1,9 @@
-import React, { useContext, useState, useMemo, useRef, useEffect } from 'react';
+import React, { useRef } from 'react';
 import styled, { withTheme } from 'styled-components';
 import { Header, Founder, Mission } from 'src/components/AboutUs';
-import { useIsReady } from 'src/utils/hooks/useIsReady';
 
 const AboutUs = () => {
   const founderRef = useRef(null);
-  // const { isReady } = useIsReady();
-  useIsReady();
-  // useEffect(() => {
-  //   document.body.style.overflowX = 'hidden';
-  //   return () => {
-  //     document.body.style.overflowX = 'visible';
-  //   };
-  // });
 
   return (
     <Container>
