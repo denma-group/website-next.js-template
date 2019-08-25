f8487519d551ca28273c5370823fc3e7889d13ff
Robert Molina <rmolinamir@gmail.com>
2019-08-25 13:54:44 -0400

    Removed useCallback to gain performance

--- src/utils/hooks/useTranslateContext/index.js
+++ src/utils/hooks/useTranslateContext/index.js
@@ -5,33 +5,56 @@ import _ from 'lodash';
 /*
 	`useTranslateContent` takes a multiplier and returns the translateY value
 */
+// Throtheled function that gets the Y translate value
+const getTranslateYValue = (startingY, componentHeight, multipliersY, setTranslateYVal) => {
+  const { scrollY, innerHeight } = window;
+  // Checks if component is in view before doing calculations for performance
+  if (scrollY + innerHeight >= startingY * 0.8 && scrollY < startingY * 1.2) {
+    // 'componentOffsetMultiplier' handles some weird cases when scrollY is less than the innerHeight
+    const componentOffsetMultiplier = _.clamp(1 - scrollY / innerHeight, 0, 1);
+    console.log('startingY', startingY);
+    const relativeY =
+      scrollY + innerHeight - startingY - componentOffsetMultiplier * (componentHeight / 2);
+
+    const calculation = relativeY * -multipliersY;
+    setTranslateYVal(calculation);
+  }
+};
 
 export const useTranslateContent = (multipliersY, { startingY, componentHeight }, isReady = true) => {
   const [translateYVal, setTranslateYVal] = useState(0);
 
   // Throtheled function that gets the Y translate value
-  const getTranslateYValue = useCallback(() => {
-    const { scrollY, innerHeight } = window;
-    // Checks if component is in view before doing calculations for performance
-    if (scrollY + innerHeight >= startingY * 0.8 && scrollY < startingY * 1.2) {
-      // 'componentOffsetMultiplier' handles some weird cases when scrollY is less than the innerHeight
-      const componentOffsetMultiplier = _.clamp(1 - scrollY / innerHeight, 0, 1);
-      const relativeY =
-        scrollY + innerHeight - startingY - componentOffsetMultiplier * (componentHeight / 2);
+  // const getTranslateYValue = () => {
+  //   const { scrollY, innerHeight } = window;
+  //   // Checks if component is in view before doing calculations for performance
+  //   if (scrollY + innerHeight >= startingY * 0.8 && scrollY < startingY * 1.2) {
+  //     // 'componentOffsetMultiplier' handles some weird cases when scrollY is less than the innerHeight
+  //     const componentOffsetMultiplier = _.clamp(1 - scrollY / innerHeight, 0, 1);
+  //     console.log('startingY', startingY);
+  //     const relativeY =
+  //       scrollY + innerHeight - startingY - componentOffsetMultiplier * (componentHeight / 2);
 
-      const calculation = relativeY * -multipliersY;
-      setTranslateYVal(calculation);
-    }
-  }, [componentHeight, multipliersY, startingY]);
+  //     const calculation = relativeY * -multipliersY;
+  //     setTranslateYVal(calculation);
+  //   }
+  // };
 
   // throttled for performance
-  const throttled = _.throttle(getTranslateYValue, 100);
+  const throttled = _.throttle(() => getTranslateYValue(startingY, componentHeight, multipliersY, setTranslateYVal), 100);
   useEffect(() => {
     if (isReady) {
-      window.addEventListener('scroll', throttled, true);
+      window.addEventListener('scroll', throttled);
       return () => window.removeEventListener('scroll', throttled);
     }
-  }, [getTranslateYValue, throttled, isReady]);
+  }, [throttled, startingY, componentHeight, multipliersY, setTranslateYVal, isReady]);
+
+  useEffect(() => {
+    if (isReady) {
+      console.log('throttling');
+      throttled();
+    }
+  }, [isReady])
 
   return { translateYVal };
 };
